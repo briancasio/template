93aa3429917b2c4e91b33fd4041569726865cb12
briancasio <113497684+briancasio@users.noreply.github.com>
2024-06-05 20:15:13 -0600

    Create MySecondProgram.java

---
+++ MySecondProgram.java
@@ -0,0 +1,46 @@
+public class MySecondProgram {
+    public static void main (String[]args){
+
+      //(1) Define two String variables and provide any value that you want
+
+
+
+      //(2) Define two numbers (one of them must be a decimal number)
+
+
+
+      /*(3) Define a variable called third that will hold the third character from ...
+         ...the first String defined in (1)
+      */
+
+
+
+      /*(4) Define a variable called sum that will store the addition of the two ...
+         ...numbers that you defined in (2)
+      */
+
+
+
+      /*(5) Define a variable called second that will contain the value of second ...
+         ...String from (1). The content of the second character must be in uppercase.
+      */
+
+
+
+      /*(6) Define a String variable called password that will be composed of the following:
+      a: the first character from the second String
+      b: the second character form the second String
+      c: the first number
+      d: the last character from the first String
+      e: the second number
+      f: a special character of your preference
+      */
+
+
+
+      //Println the results
+
+
+
+    }
+}
